de79f85a34e3b19afaab97f0d9f44bfaf1b8d208
Farhan Sadiq <farhansadiq2021@gmail.com>
2026-06-02 12:01:38 +0600

    Added the top generations section in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Banner from "@/components/Banner";
+import TopGenerations from "@/components/TopGenerations";
 
 export default function Home() {
   return (
     <div className="max-w-6xl mx-auto ">
       <Banner />
-      {/* <TopGenerations/> */}
+      <TopGenerations/>
     </div>
   );
 }
---
+++ src/components/TopGenerations.jsx
@@ -0,0 +1,19 @@
+import PhotoCard from "./PhotoCard";
+
+export default async function TopGenerations() {
+  const res = await fetch("https://pixgen-roan.vercel.app/data.json");
+  const data = await res.json();
+  const topData = data.slice(0, 8);
+  console.log(topData);
+
+  return (
+    <div>
+      <h2 className="font-bold text-2xl my-6 text-center">Top Generations</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {topData.map((photo) => {
+          return <PhotoCard key={photo.id} photo={photo} />;
+        })}
+      </div>
+    </div>
+  );
+}
